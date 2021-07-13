c70218218ab91cf9cb7d79e2daa7f8be61a476bf
Ajose Tosin <tosinemidastouch@gmail.com>
2021-07-13 02:37:19 +0100

    woring on promise

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-const promise = new Promise((resolve, reject) => {
-if (true) {
-    resolve('stuff worked')
-} else {
-    reject('Error, it broke')
-}
-})
+// const promise = new Promise((resolve, reject) => {
+// if (true) {
+//     resolve('stuff worked')
+// } else {
+//     reject('Error, it broke')
+// }
+// })
 
-promise.then(result => console.log(result));
+// promise
+// .then(result => console.log(result + '!'))
+// .then(result2 => {
+//     throw Error
+//     console.log(result2);
+// })
+// .catch(() => console.log(('error!')));
+
+promise
+.then(result => console.log(result + '!'))
+.then(result2 => {
+    throw Error
+    console.log(result2);
+})
+.catch(() => console.log(('error!')));
