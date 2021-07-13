58c4c7f23c2e0d459a9480451c1b513c0cc72489
Ajose Tosin <tosinemidastouch@gmail.com>
2021-07-13 16:16:33 +0100

    tutorial on fetch

--- app.js
+++ app.js
@@ -14,10 +14,39 @@
 // })
 // .catch(() => console.log(('error!')));
 
-promise
-.then(result => console.log(result + '!'))
-.then(result2 => {
-    throw Error
-    console.log(result2);
-})
-.catch(() => console.log(('error!')));
+// promise
+// .then(result => result + '!')
+// .then(result2 => result2 + '?')
+// .catch(() => console.log('error!'))
+// .then(result3 => {
+//     console.log(result3 + '!');
+// })
+
+// const promise2 = new Promise((resolve, reject) => {
+//         setTimeout(resolve,100, 'Hiii')
+// })
+// const promise3 = new Promise((resolve, reject) => {
+//         setTimeout(resolve,1000, 'POOKIE')
+// })
+// const promise4 = new Promise((resolve, reject) => {
+//         setTimeout(resolve,3000, 'Is it me you are looking for?')
+// })
+
+// Promise.all([promise, promise2, promise3, promise4])
+// .then(values =>{
+//     console.log(values);
+// })
+
+const urls = [
+    'https://jsonplaceholder.typicode.com/users',
+    'https://jsonplaceholder.typicode.com/posts',
+    'https://jsonplaceholder.typicode.com/albums'
+]
+
+Promise.all(urls.map((url) => {
+    return fetch(url).then(resp => resp.jason())
+})).then(results => {
+console.log(results[0]);
+console.log(results[1]);
+console.log(results[2]);
+}).catch(() => console.log('error'));
