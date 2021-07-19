8650378aed00dd36372f6cf4ffccd933cce9fad6
Ajose Tosin <tosinemidastouch@gmail.com>
2021-07-19 17:50:17 +0100

    Started working on asyn await

--- app.js
+++ app.js
@@ -37,29 +37,123 @@
 //     console.log(values);
 // })
 
-const urls = [
-    'https://jsonplaceholder.typicode.com/users',
-    'https://jsonplaceholder.typicode.com/posts',
-    'https://jsonplaceholder.typicode.com/albums'
-]
 
-Promise.all(urls.map((url) => {
-    return fetch(url).then(resp => resp.jason())
-})).then(results => {
-console.log(results[0]);
-console.log(results[1]);
-console.log(results[2]);
-}).catch(() => console.log('error'));
+/* Type 1 */
+// const urls = [
+//     'https://jsonplaceholder.typicode.com/users',
+//     'https://jsonplaceholder.typicode.com/posts',
+//     'https://jsonplaceholder.typicode.com/albums'
+// ]
+
+// Promise.all(urls.map((url) => {
+//     return fetch(url).then(resp => resp.jason())
+// })).then(results => {
+// console.log(results[0]);
+// console.log(results[1]);
+// console.log(results[2]);
+// }).catch(() => console.log('error'));
+
+// /* Type 2 */
+// const urls = [
+//     'https://jsonplaceholder.typicode.com/users',
+//     'https://jsonplaceholder.typicode.com/posts',
+//     'https://jsonplaceholder.typicode.com/albums'
+// ]
+
+
+// const urls = [
+//     'http://swapi.dev/api/people/1',
+//     'http://swapi.dev/api/people/2',
+//     'http://swapi.dev/api/people/3',
+//     'http://swapi.dev/api/people/4'
+//   ]
+
+// Promise.all(urls.map(url =>
+//     fetch(url).then(people => people.json())
+// ))
+//   .then(array => {
+//     console.log('1', array[0])
+//     console.log('2', array[1])
+//     console.log('3', array[2])
+//     console.log('4', array[3])
+//   })
+//   .catch(err => console.log('ughhhh fix it!', err));
+
 
 /* ASYNC AWAIT */
-//Not clean
-fetch('https://jsonplaceholder.typicode.com/users')
-.then(resp => resp.jason())
-.then(console.log());
+// Not clean
+// fetch('https://jsonplaceholder.typicode.com/users')
+// .then(resp => resp.jason())
+// .then(console.log());
 
-//Pretty much better
-async function fetchUsers() {
-    const resp = await fetch('https://jsonplaceholder.typicode.com/users')
-    const data = await resp.jason();
-    console.log(data);
+ //Pretty much better
+// async function fetchUsers() {
+//     const resp = await fetch('https://jsonplaceholder.typicode.com/users')
+//     const data = await resp.json();
+//     console.log(data);
+// }
+
+// fetchUsers()
+
+// const urls = [
+//     'https://jsonplaceholder.typicode.com/users',
+//     'https://jsonplaceholder.typicode.com/posts',
+//     'https://jsonplaceholder.typicode.com/albums'
+// ]
+
+const getData = async function() {
+    try {
+        const [users, posts, albums] = await Promise.all(urls.map(url =>
+            fetch(url).then(resp => resp.json())
+            ))
+    console.log('users', users);
+    console.log('posts', posts);
+    console.log('albums', albums);
+    } catch {
+        console.log('oops');
+    }
+
 }
+
+// Solve the below problems:
+
+// #1) Convert the below promise into async await
+fetch("https://jsonplaceholder.typicode.com/users/")
+  .then((response) => response.json())
+  .then(console.log);
+
+  /* Answer */
+  async function getUser() {
+    const response = await fetch("https://jsonplaceholder.typicode.com/users/");
+    const data = await response.json();
+    console.log(data);
+  }
+
+  getUser()
+
+
+/*ES2018 Object Spread Operator */
+const array = [1,2,3,4,5]
+function sum (a,b,c,d,e){
+    return a + b + c + d + e;
+}
+
+sum(... array) //15
+
+const animals = {
+    tiger: 23,
+    lion: 5,
+    monkey:2,
+    birds: 40
+}
+
+function objectSpread(p1, p2, p3){
+    console.log(p1);
+    console.log(p2);
+    console.log(p3);
+}
+
+const {tiger, lion, ...rest} = animals;
+
+objectSpread(tiger, lion, rest);
+
